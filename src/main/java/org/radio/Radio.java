4688382797b508fe97320790b949054b65792458
Kristina Krutova <fruktovaya99@mail.ru>
2023-02-17 01:09:24 +0300

    Initial commit

---
+++ src/main/java/org/radio/Radio.java
@@ -0,0 +1,40 @@
+package org.radio;
+
+public class Radio {
+    public int currentVolume;
+    public int currentStation;
+
+    public void setStation(int station) {
+        if (station >= 0 && station <= 9) {
+            currentStation = station;
+        }
+    }
+
+    public void next() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,114 @@
+import org.junit.jupiter.api.Test;
+import org.radio.Radio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+    @Test
+    void nextStation(){
+        Radio radio = new Radio();
+        radio.currentStation = 0;
+        radio.next();
+
+        int exp = 1;
+        int act = radio.currentStation;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void prevStation(){
+        Radio radio = new Radio();
+        radio.currentStation = 1;
+        radio.prev();
+
+        int exp = 0;
+        int act = radio.currentStation;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void prevFirstStation(){
+        Radio radio = new Radio();
+        radio.currentStation = 0;
+        radio.prev();
+
+        int exp = 9;
+        int act = radio.currentStation;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void nextLastStation(){
+        Radio radio = new Radio();
+        radio.currentStation = 9;
+        radio.next();
+
+        int exp = 0;
+        int act = radio.currentStation;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void setStation(){
+        Radio radio = new Radio();
+        radio.currentStation = 9;
+        radio.setStation(5);
+
+        int exp = 5;
+        int act = radio.currentStation;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void incVolume(){
+        Radio radio = new Radio();
+        radio.currentVolume = 0;
+        radio.increaseVolume();
+
+        int exp = 1;
+        int act = radio.currentVolume;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void decVolume(){
+        Radio radio = new Radio();
+        radio.currentVolume = 1;
+        radio.decreaseVolume();
+
+        int exp = 0;
+        int act = radio.currentVolume;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void decZeroVolume(){
+        Radio radio = new Radio();
+        radio.currentVolume = 0;
+        radio.decreaseVolume();
+
+        int exp = 0;
+        int act = radio.currentVolume;
+
+        assertEquals(exp, act);
+    }
+
+    @Test
+    void incMaxVolume(){
+        Radio radio = new Radio();
+        radio.currentVolume = 10;
+        radio.increaseVolume();
+
+        int exp = 10;
+        int act = radio.currentVolume;
+
+        assertEquals(exp, act);
+    }
+}
